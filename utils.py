af5c139b93e35052e2fadc8d9f6f94b87ef2937c
Vinokurov Alexander <aleksandr.vinokurov@infotecs.ru>
2018-05-10 00:12:00 +0300

    typing for generate_random_str function

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def generate_random_str(length=8):
+def generate_random_str(length: int=8) -> str:
     return ''.join(
         random.choice(string.ascii_letters + string.digits) for _ in range(length)
     )
